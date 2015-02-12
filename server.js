e0e2c7a8285b51d9754366fcf5b082a0a7c516e7
Fernando Teodoro de Lima <ferfo.lima@gmail.com>
2015-02-12 02:20:43 -0200

    webpage test

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get('/subscribe', function(req, res) {
                 }
             });
             socket.on('disconnect', function () {
-                clearInterval(tweets);
+                socket.emit('disconnected');
             });
         });
     }
