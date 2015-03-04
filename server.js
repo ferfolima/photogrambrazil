928e31c7aca15394299543571bed172e5610e851
Fernando Teodoro de Lima <fernando@Fernandos-MacBook-Pro.local>
2015-03-04 01:12:17 -0300

    sem remover fotos

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ app.post('/callback', function(req, res) {
     // concatenate to the url and send as a argument to the client side
     data.forEach(function(tag) {
       self.tagid = tag.id;
-      console.log("\n" + tag.id);
+      console.log("\n" + tag);
       var url = 'https://api.instagram.com/v1/tags/' + tag.object_id + '/media/recent?client_id=' + clientID;
       sendMessage(url);
 
