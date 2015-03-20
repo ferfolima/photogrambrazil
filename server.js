b7c217b7cc5d52d9bd10ccf8cfdac21eaca89552
Fernando Teodoro de Lima <fernando@Fernandos-MacBook-Pro.local>
2015-03-20 18:50:56 -0300

    thumbnail

--- server.js
+++ server.js
@@ -98,14 +98,17 @@ app.get('/subscribe', function(req, res) {
     parsedRequest = url.parse(req.url, true);
     if (parsedRequest['query']['hub.tag'] != null && parsedRequest['query']['hub.tag'].length > 0) {
         self.hashtag = parsedRequest['query']['hub.tag'];
-        self.tagid = Instagram.subscriptions.subscribe({
-                        object: 'tag',
-                        object_id: self.hashtag,
-                        aspect: 'media',
-                        callback_url: 'http://test-gram.herokuapp.com/callback',
-                        type: 'subscription',
-                        id: '#'
-                    });
+        var jsonSubscription = Instagram.subscriptions.subscribe({
+            object: 'tag',
+            object_id: self.hashtag,
+            aspect: 'media',
+            callback_url: 'http://test-gram.herokuapp.com/callback',
+            type: 'subscription',
+            id: '#'
+        });
+
+        var parsedJson = JSON.parse(jsonSubscription);
+        self.tagid = parsedJson.id;
 
         io.sockets.once('connection', function (socket) {
             Instagram.tags.recent({
@@ -148,6 +151,7 @@ app.post('/callback', function(req, res) {
     // Grab the hashtag "tag.object_id"
     // concatenate to the url and send as a argument to the client side
     data.forEach(function(tag) {
+      //self.tagid = tag.subscription_id;
       var url = 'https://api.instagram.com/v1/tags/' + tag.object_id + '/media/recent?client_id=' + clientID;
       sendMessage(url);
 
