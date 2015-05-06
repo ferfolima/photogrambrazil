7ee26d1a9b60a11ab6389c311811f297713a298e
Fernando <ferfo.lima@gmail.com>
2015-05-05 23:50:50 -0300

    unsubscribe

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get('/subscribe', function(req, res) {
     parsedRequest = url.parse(req.url, true);
     if (parsedRequest['query']['hub.tag'] != null && parsedRequest['query']['hub.tag'].length > 0) {
         var hashtag = parsedRequest['query']['hub.tag'];
-        var jsonSubscription = Instagram.tags.subscribe({
+        var jsonSubscription = Instagram.subscriptions.subscribe({
             object: 'tag',
             object_id: hashtag,
             aspect: 'media',
@@ -131,20 +131,19 @@ app.get('/unsubscribe', function(req, res) {
     var parsedRequest = url.parse(req.url, true);
     if (parsedRequest['query']['hub.tag'] != null && parsedRequest['query']['hub.tag'].length > 0) {
         var hashtag = parsedRequest['query']['hub.tag'];
-        var jsonSubscription = Instagram.tags.subscribe({
+        var jsonSubscription = Instagram.subscriptions.subscribe({
             object: 'tag',
             object_id: hashtag,
             aspect: 'media',
             callback_url: 'http://photogrambrazil.herokuapp.com/callback',
             type: 'subscription',
-            id: '#',
-            complete: function(data){
-                var tagid = JSON.parse(data).id;
-                console.log("\n\n\n" + "\n\tJSON: " + data + "\n\n\n");
-                console.log("\n\n\n" + "\n\tID: " + tagid + "\n\n\n");
-                Instagram.tags.unsubscribe({id: tagid});    
-            }
+            id: '#'
         });
+
+        var tagid = JSON.parse(jsonSubscription).id;
+        console.log("\n\n\n" + "\n\tJSON: " + jsonSubscription + "\n\n\n");
+        console.log("\n\n\n" + "\n\tID: " + tagid + "\n\n\n");
+        Instagram.tags.unsubscribe({id: tagid});
     }
 
     res.redirect('http://photogrambrazil.herokuapp.com');
