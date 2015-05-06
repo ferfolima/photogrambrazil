e5052d13823c9e2a922f79750cb4e83ffd2f95b8
Fernando <ferfo.lima@gmail.com>
2015-05-06 02:00:04 -0300

    unsubscribe

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ app.get('/unsubscribe', function(req, res) {
         var hashtag = parsedRequest['query']['hub.tag'];
         // Instagram.subscriptions.unsubscribe({object: 'tag', id: null});
         JSON.parse(Instagram.subscriptions.list(), function(result){
-            console.log("O tal do ID é: " + result + "entendeu?");
+            console.log("O tal do ID é: " + result[1].id + "entendeu?");
         });
 
         // JSON.parse(jsonSubscription).id;
