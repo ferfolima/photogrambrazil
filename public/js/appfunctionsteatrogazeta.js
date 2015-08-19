c36ba321aac6d4322a51a4657ed73a84ca003716
unknown <ferfo.lima@gmail.com>
2015-08-18 22:24:25 -0300

    teatro gazeta

--- public/js/appfunctionsteatrogazeta.js
+++ public/js/appfunctionsteatrogazeta.js
@@ -33,8 +33,8 @@ $(function(){
            if($(this).val()=='Assinar'){
                $form.attr('action','http://photogrambrazil.heroku.com/teatrogazeta/subscribe/');
                localStorage.setItem('user', 'visited');
-               //hashTag = $('form[name="chooseHash"] input[type="text"]').val();
-               window.open('http://photogrambrazil.heroku.com/teatrogazeta/slideshow/' + 'teste')
+               var $hashTag = $('form[name="chooseHash"] input[type="text"]').val();
+               window.open('http://photogrambrazil.heroku.com/teatrogazeta/slideshow/' + $hashTag);
            }
             else{
                 $form.attr('action','http://photogrambrazil.heroku.com/teatrogazeta/unsubscribe/');
