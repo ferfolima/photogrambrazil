ae461ab611eb5b00e2f28d656de80300829bb22b
unknown <ferfo.lima@gmail.com>
2015-09-01 00:08:49 -0300

    2x2

--- public/js/slideShowImgiguana.js
+++ public/js/slideShowImgiguana.js
@@ -12,7 +12,7 @@
          * [Application initialization method / call for the methods being initializated in order]
          */
         init: function() {
-            this.count = 1;
+            count = 0;
             this.mostRecent();
         },
 
@@ -29,14 +29,14 @@
                     source = $('#slideShow-tpl').html(),
                     compiledTemplate = Handlebars.compile(source),
                     result = compiledTemplate({insert: query});
-                if(this.count % 2 == 0){
+                if(count % 2 == 0){
                   var imgWrap = $('#slider');
                 }
                 else {
                   var imgWrap = $('#slider2');
                 }
                 results.push(result);
-                populateSlider(results,this.count);
+                populateSlider(results,count);
 
                 var sPageURL = window.location.search.substring(1);
                 var sParameterName = sPageURL.split('=');
@@ -45,10 +45,10 @@
                     var elements = document.getElementsByClassName('hashtagTitle');
                     for (var i = 0; i < elements.length; i++) {
                       // elements[i].innerHTML = '#' + sParameterName[1];
-                      elements[i].innerHTML = '#' + this.count;
+                      elements[i].innerHTML = '#' + count;
                     }
                 }
-                this.count++;
+                count++;
             });
         }
 
